3bb4b62cace7910c175ed85c8e8ebd0595e31ea1
Rajeev Jasti <inboxtorj@gmail.com>
2026-09-03 23:45:18 -0400

    add resume

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rj-portfolio-v23';
+const CACHE_NAME = 'rj-portfolio-v25';
 const CACHE_EXPIRATION_DAYS = 7;
 const CACHE_EXPIRATION_MS = CACHE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000;
 
